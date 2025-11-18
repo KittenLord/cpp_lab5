22796a9d19f86337372a85ab98eca76781f48f03
KittenLord <vanya.braslavets@gmail.com>
2025-11-18 23:54:08 +0200

    remake for lab5

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
+#include <clocale>
+#include <cstdlib>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdint.h>
+
+#include <thread>
+#include <mutex>
+#include <vector>
 
 #include "protocol.cpp"
 
@@ -13,18 +19,311 @@
 
 #define PORT 6969
 
+typedef struct {
+    char *username;
+    UserId userId;
+    int socket;
+
+    std::mutex lock;
+} UserInfo;
+
+std::vector<UserInfo *> users;
+std::mutex usersLock;
+
+unsigned int rngSeed = 916293128;
+std::mutex rngSeedLock;
+
+void threadRoutine(int sock) {
+    UserInfo *info = (UserInfo *)calloc(sizeof(UserInfo), 1);
+    info->socket = sock;
+
+    bool notLoggedIn = true;
+
+    while(true) {
+        struct Header header;
+        size_t bytesRead = read(sock, &header, sizeof(Header));
+        if(bytesRead != sizeof(Header)) goto cleanup;
+
+        switch(header.type) {
+            case PACKET_CLIENT_LOGIN:
+                {
+                    if(!notLoggedIn) goto cleanup; // TODO: either rename or return the same userid and do nothing
+
+                    auto packet = header.client_login;
+                    info->username = (char *)calloc(sizeof(char), packet.usernameLength + 1);
+                    size_t bytesRead = read(sock, info->username, packet.usernameLength);
+                    if(bytesRead != packet.usernameLength) {
+                        free(info->username);
+                        goto cleanup;
+                    }
+
+                    bool badUsername = false;
+                    if(badUsername) {
+                        free(info->username);
+
+                        struct Header response = {
+                            .type = PACKET_SERVER_LOGIN,
+                            .server_login = {
+                                .error = ERROR_BAD_USERNAME,
+                            },
+                        };
+
+                        info->lock.lock();
+                            write(sock, &response, sizeof(Header));
+                        info->lock.unlock();
+                        continue;
+                    }
+
+                    rngSeedLock.lock();
+                        uint32_t a = rand_r(&rngSeed);
+                        uint32_t b = rand_r(&rngSeed);
+                        info->userId = (UserId)a | ((UserId)b << 32);
+                    rngSeedLock.unlock();
+
+                    usersLock.lock();
+                        users.push_back(info);
+                    usersLock.unlock();
+
+                    struct Header response = {
+                        .type = PACKET_SERVER_LOGIN,
+                        .server_login = {
+                            .error = ERROR_SUCCESS,
+                            .userId = info->userId,
+                        },
+                    };
+
+                    info->lock.lock();
+                        write(info->socket, &response, sizeof(Header));
+                    info->lock.unlock();
+                }
+                break;
+            case PACKET_CLIENT_MESSAGE_ALL:
+                if(notLoggedIn) goto cleanup;
+
+                {
+                    auto packet = header.client_messageAll;
+                    char *message = (char *)calloc(sizeof(char), packet.messageLength + 1);
+                    size_t bytesRead = read(sock, message, packet.messageLength);
+                    if(bytesRead != packet.messageLength) {
+                        free(message);
+                        goto cleanup;
+                    }
+
+                    struct Header notification = {
+                        .type = PACKET_SERVER_RECEIVE_MESSAGE,
+                        .server_receiveMessage = {
+                            .isGlobalMessage = true,
+                            .userId = info->userId,
+
+                            .messageLength = packet.messageLength,
+                        },
+                    };
+
+                    usersLock.lock();
+                    {
+                        for(auto &user : users) {
+                            if(user->userId == info->userId) continue;
+
+                            user->lock.lock();
+                                write(user->socket, &notification, sizeof(Header));
+                                write(user->socket, message, packet.messageLength);
+                            user->lock.unlock();
+                        }
+                    }
+                    usersLock.unlock();
+
+                    free(message);
+
+                    struct Header response = {
+                        .type = PACKET_SERVER_MESSAGE,
+                        .server_message = {
+                            .error = ERROR_SUCCESS,
+                        },
+                    };
+
+                    info->lock.lock();
+                        write(info->socket, &response, sizeof(Header));
+                    info->lock.unlock();
+                }
+
+                break;
+            case PACKET_CLIENT_LIST_USERS:
+                {
+                    // NOTE: PacketClient_ListUsers contains no data
+
+                    struct Header response;
+                    UserId *array;
+
+                    usersLock.lock();
+                    {
+                        response = {
+                            .type = PACKET_SERVER_LIST_USERS,
+                            .server_listUsers = {
+                                .amount = users.size(),
+                            },
+                        };
+
+                        array = (UserId *)calloc(sizeof(UserId), users.size());
+                        size_t index = 0;
+
+                        for(auto &user : users) {
+                            array[index] = user->userId;
+                            index += 1;
+                        }
+                    }
+                    usersLock.unlock();
+
+                    info->lock.lock();
+                        write(info->socket, &response, sizeof(Header));
+                        write(info->socket, array, sizeof(UserId) * response.server_listUsers.amount);
+                    info->lock.unlock();
+
+                    free(array);
+                }
+                break;
+            case PACKET_CLIENT_GET_USERNAME:
+                {
+                    auto packet = header.client_getUsername;
+
+                    char *username = nullptr;
+                    size_t len;
+
+                    usersLock.lock();
+                    {
+                        for(auto &user : users) {
+                            if(user->userId == packet.userId) {
+                                len = strlen(user->username);
+                                username = (char *)calloc(sizeof(char), len + 1);
+                                memcpy(username, user->username, len);
+                                break;
+                            }
+                        }
+                    }
+                    usersLock.unlock();
+
+                    info->lock.lock();
+                        if(!username) {
+                            struct Header response = {
+                                .type = PACKET_SERVER_GET_USERNAME,
+                                .server_getUsername = {
+                                    .error = ERROR_UNKNOWN_USER_ID,
+                                    .usernameLength = 0,
+                                },
+                            };
+
+                            write(info->socket, &response, sizeof(Header));
+                        }
+                        else {
+                            struct Header response = {
+                                .type = PACKET_SERVER_GET_USERNAME,
+                                .server_getUsername = {
+                                    .error = ERROR_SUCCESS,
+                                    .usernameLength = len,
+                                },
+                            };
+
+                            write(info->socket, &response, sizeof(Header));
+                            write(info->socket, username, len);
+                        }
+                    info->lock.unlock();
+
+                    if(username) free(username);
+                }
+                break;
+            case PACKET_CLIENT_MESSAGE:
+                if(notLoggedIn) goto cleanup;
+
+                {
+                    auto packet = header.client_message;
+
+                    char *message = (char *)calloc(sizeof(char), packet.messageLength + 1);
+                    size_t bytesRead = read(info->socket, message, packet.messageLength);
+                    if(bytesRead != packet.messageLength) {
+                        free(message);
+                        goto cleanup;
+                    }
+
+                    bool success = false;
+
+                    // NOTE: i really dont like the double lock here, but we can't really do anything with it
+                    usersLock.lock();
+                    {
+                        for(auto &user : users) {
+                            if(user->userId == packet.userId) {
+                                success = true;
+
+                                user->lock.lock();
+                                {
+                                    struct Header notification = {
+                                        .type = PACKET_SERVER_RECEIVE_MESSAGE,
+                                        .server_receiveMessage = {
+                                            .isGlobalMessage = false,
+                                            .userId = info->userId,
+
+                                            .messageLength = packet.messageLength,
+                                        }
+                                    };
+
+                                    write(user->socket, &notification, sizeof(Header));
+                                    write(user->socket, message, packet.messageLength);
+                                }
+                                user->lock.unlock();
+                            }
+                        }
+                    }
+                    usersLock.unlock();
+
+                    free(message);
+
+                    info->lock.lock();
+                    {
+                        if(success) {
+                            struct Header response = {
+                                .type = PACKET_SERVER_MESSAGE,
+                                .server_message = {
+                                    .error = ERROR_SUCCESS,
+                                },
+                            };
+
+                            write(info->socket, &response, sizeof(Header));
+                        }
+                        else {
+                            struct Header response = {
+                                .type = PACKET_SERVER_MESSAGE,
+                                .server_message = {
+                                    .error = ERROR_UNKNOWN_USER_ID,
+                                },
+                            };
+
+                            write(info->socket, &response, sizeof(Header));
+                        }
+                    }
+                    info->lock.unlock();
+                }
+                break;
+            default:
+                goto cleanup;
+                break;
+        }
+    }
+
+cleanup:
+    close(sock);
+    return;
+}
+
 int main(void) {
     int result = 0;
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
+    if(sock == -1) {
         printf("ERROR: socket\n");
         return 1;
     }
 
     int reuseAddrOptions = 1;
     result = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseAddrOptions, sizeof(reuseAddrOptions));
-    if (result == -1) {
+    if(result == -1) {
         printf("ERROR: reuse\n");
         return 1;
     }
@@ -38,20 +337,20 @@ int main(void) {
     };
 
     result = bind(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-    if (result == -1) {
+    if(result == -1) {
         printf("ERROR: bind\n");
         return 1;
     }
 
     result = listen(sock, 10);
-    if (result == -1) {
+    if(result == -1) {
         printf("ERROR: listen\n");
         return 1;
     }
 
     printf("SERVER STARTED\n");
 
-    while (1) {
+    while(true) {
         struct sockaddr_in clientAddr    = {0};
         socklen_t          clientAddrLen = sizeof(clientAddr);
 
@@ -63,58 +362,8 @@ int main(void) {
 
         printf("CLIENT CONNECTED\n");
 
-        // NOTE: guaranteed to be able to hold all 3 headers
-        union PacketMax headerBuffer;
-
-        PacketMode packetMode;
-        read(clientSock, &packetMode, sizeof(packetMode));
-
-        switch(packetMode) {
-            case PACKET_1:
-                {
-                    printf("Received message from Client 1\n");
-
-                    read(clientSock, ((uint8_t *)&headerBuffer) + sizeof(PacketMode), sizeof(struct Packet1) - sizeof(PacketMode));
-                    struct Packet1 p = headerBuffer.p1;
-
-                    printf("Client's RAM size: %d\n", p.ramSize);
-                    printf("Client's external drive availability: %d\n", p.diskAvailable);
-                    printf("Client's screen width: %d\n", p.screenWidth);
-                }
-                break;
-            case PACKET_2:
-                {
-                    printf("Received message from Client 2\n");
-
-                    read(clientSock, ((uint8_t *)&headerBuffer) + sizeof(PacketMode), sizeof(struct Packet2) - sizeof(PacketMode));
-                    struct Packet2 p = headerBuffer.p2;
-
-                    printf("Client's line height: %d\n", p.lineHeight);
-                    printf("Client's notification panel width: %d\n", p.notifWidth);
-                    printf("Client's horizontal DPI: %d\n", p.dpiHorizontal);
-                }
-                break;
-            case PACKET_3:
-                {
-                    printf("Received message from Client 3\n");
-
-                    read(clientSock, ((uint8_t *)&headerBuffer) + sizeof(PacketMode), sizeof(struct Packet3) - sizeof(PacketMode));
-                    struct Packet3 p = headerBuffer.p3;
-
-                    char *buffer = (char *)calloc(sizeof(char), p.length);
-                    read(clientSock, buffer, p.length);
-
-                    printf("Client's message: %s\n", buffer);
-
-                    free(buffer);
-                }
-                break;
-            default:
-                printf("INVALID PACKET TYPE: %d\n", packetMode);
-                break;
-        }
-
-        close(clientSock);
+        std::thread t(threadRoutine, clientSock);
+        t.detach();
     }
 
     close(sock);
--- protocol.cpp
+++ protocol.cpp
@@ -2,37 +2,99 @@
 #define __PROTOCOL
 
 #include <stdint.h>
+#include <unistd.h>
 
-typedef uint8_t PacketMode;
+typedef uint8_t PacketType;
+#define PACKET_CLIENT_LOGIN 1
+#define PACKET_CLIENT_MESSAGE_ALL 2
+#define PACKET_CLIENT_LIST_USERS 3
+#define PACKET_CLIENT_GET_USERNAME 4
+#define PACKET_CLIENT_MESSAGE 5
 
-#define PACKET_1 1
-#define PACKET_2 2
-#define PACKET_3 3
+#define PACKET_SERVER_LOGIN 101
+#define PACKET_SERVER_MESSAGE 102
+#define PACKET_SERVER_RECEIVE_MESSAGE 103
+#define PACKET_SERVER_LIST_USERS 104
+#define PACKET_SERVER_GET_USERNAME 105
 
-struct Packet1 {
-    uint8_t mode; // ALWAYS 1
-    uint32_t ramSize;
-    uint32_t diskAvailable;
-    uint32_t screenWidth;
+typedef uint8_t ErrorType;
+#define ERROR_SUCCESS 0
+#define ERROR_UNKNOWN_USER_ID 1
+#define ERROR_BAD_MESSAGE 2
+#define ERROR_BAD_USERNAME 3
+
+typedef uint64_t UserId;
+
+
+
+struct PacketClient_Login {
+    size_t usernameLength;
+    // extra `usernameLength` bytes
 };
 
-struct Packet2 {
-    uint8_t mode; // ALWAYS 2
-    uint32_t lineHeight;
-    uint32_t notifWidth;
-    uint32_t dpiHorizontal;
+struct PacketClient_MessageAll {
+    size_t messageLength;
+    // extra `messageLength` bytes
 };
 
-struct Packet3 {
-    uint8_t mode; // ALWAYS 3
-    uint64_t length;
-    // After sending Packet3, we MUST send `length` bytes
+struct PacketClient_ListUsers {
 };
 
-union PacketMax {
-    Packet1 p1;
-    Packet2 p2;
-    Packet3 p3;
+struct PacketClient_GetUsername {
+    UserId userId;
+};
+
+struct PacketClient_Message {
+    UserId userId;
+    size_t messageLength;
+    // extra `messageLength` bytes
+};
+
+
+
+struct PacketServer_Login {
+     ErrorType error;
+     UserId userId;
+};
+
+struct PacketSercer_Message {
+    ErrorType error;
+};
+
+struct PacketServer_ReceiveMessage {
+    uint8_t isGlobalMessage;
+    UserId userId;
+
+    size_t messageLength;
+    // extra `messageLength` bytes
+};
+
+struct PacketServer_ListUsers {
+    uint64_t amount;
+    // extra `amount * sizeof(UserId)` bytes
+};
+
+struct PacketServer_GetUsername {
+    ErrorType error;
+    size_t usernameLength;
+    // extra `usernameLength` bytes
+};
+
+struct Header {
+    PacketType type;
+    union {
+        PacketClient_Login client_login; // Ask server to assign you a user id
+        PacketClient_MessageAll client_messageAll; // Broadcast a message to everyone
+        PacketClient_ListUsers client_listUsers; // Get list of user ids connected to the server
+        PacketClient_GetUsername client_getUsername; // Get username associated with a user id
+        PacketClient_Message client_message; // Attempt to send a message to a specific user
+
+        PacketServer_Login server_login; // RESPONSE to PacketClient_Login
+        PacketSercer_Message server_message; // RESPONSE to PacketClient_Message or PacketClient_MessageAll
+        PacketServer_ReceiveMessage server_receiveMessage; // Notify user of a new message
+        PacketServer_ListUsers server_listUsers; // RESPONSE to PacketClient_ListUsers
+        PacketServer_GetUsername server_getUsername; // RESPONSE to PacketClient_GetUsername
+    };
 };
 
 #endif // __PROTOCOL
